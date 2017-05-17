8a9f8fb31002120f4a61a39086cb87216cc342a0
Xingchen Hong <hello@xc-h.net>
2017-05-17 10:52:31 -0500

    Remove sample data collection.

--- meteor-app/imports/api/samples/collection.js
+++
@@ -1,8 +0,0 @@
-// Definition of the links collection
-
-import { Mongo } from 'meteor/mongo';
-
-export const Samples = new Mongo.Collection('samples');
-
-export const channelValueMin = 0;
-export const channelValueMax = 256;
--- meteor-app/imports/startup/server/fixtures.js
+++
@@ -1,8 +0,0 @@
-// Fill the DB with example data on startup
-
-import { Meteor } from "meteor/meteor";
-import { Samples } from "/imports/api/samples/collection.js";
-
-Meteor.startup(() => {
-  Samples.remove({});
-});
