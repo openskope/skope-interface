0148f598fa7f8b9dfafa99dd49c650fa18f95f9c
Xingchen Hong <hello@xc-h.net>
2017-05-17 17:36:51 -0500

    Split initial state into its own file.

--- meteor-app/imports/ui/reducers/index.js
+++ meteor-app/imports/ui/reducers/index.js
@@ -3,45 +3,9 @@ import { FlowRouter } from "meteor/kadira:flow-router";
 import * as actions from "/imports/ui/actions";
 import * as reducers from "./reducers";
 
-const defaultState = {
-  navInfo: [],
-  workspace: {
-    //! For testing only. Should be empty on start.
-    layers: [
-      {
-        name: "Water-year Precipitation",
-        urlTile: "PPT_water_year",
-        minZoom: 5,
-        maxZoom: 12,
-        invisible: false,
-        opacity: 0.7,
-        extent: "-12856096.661340367, 3620057.6595859504, -11359153.899403473, 5371382.851655904",
-      },
-      {
-        name: "Fahrenheit GDD",
-        urlTile: "GDD_may_sept_demosaic",
-        minZoom: 5,
-        maxZoom: 12,
-        invisible: false,
-        opacity: 0.7,
-        extent: "-12856096.661340367, 3620057.6595859504, -11359153.899403473, 5371382.851655904",
-      },
-    ],
+import initialState from "./initialState";
 
-    // Whether a point is selected for inspection.
-    inspectPointSelected: false,
-    // If a point is selected for inspection, this would be its coordinates in lat-long.
-    inspectPointCoordinate: [0, 0],
-    // If a point is selected for inspection, this shows if data is still being loaded.
-    inspectPointLoading: false,
-    // If a point is selected for inspection, the loaded data will be stored here.
-    inspectPointData: null,
-
-    filterValue: null,
-  },
-};
-
-export default (state = defaultState, action) => {
+export default (state = initialState, action) => {
   console.log('run reducer', action.type, {state, action});
 
   let nextState = state;
---
+++ meteor-app/imports/ui/reducers/initialState.js
@@ -0,0 +1,47 @@
+export default {
+  navInfo: [],
+
+  search: {
+    // @type {string} - search string.
+    input: "",
+    // @type {boolean} - whether the results are being loaded.
+    pending: false,
+    // @type {object|null} - search result object.
+    results: null,
+  },
+
+  workspace: {
+    //! For testing only. Should be empty on start.
+    layers: [
+      {
+        name: "Water-year Precipitation",
+        urlTile: "PPT_water_year",
+        minZoom: 5,
+        maxZoom: 12,
+        invisible: false,
+        opacity: 0.7,
+        extent: "-12856096.661340367, 3620057.6595859504, -11359153.899403473, 5371382.851655904",
+      },
+      {
+        name: "Fahrenheit GDD",
+        urlTile: "GDD_may_sept_demosaic",
+        minZoom: 5,
+        maxZoom: 12,
+        invisible: false,
+        opacity: 0.7,
+        extent: "-12856096.661340367, 3620057.6595859504, -11359153.899403473, 5371382.851655904",
+      },
+    ],
+
+    // Whether a point is selected for inspection.
+    inspectPointSelected: false,
+    // If a point is selected for inspection, this would be its coordinates in lat-long.
+    inspectPointCoordinate: [0, 0],
+    // If a point is selected for inspection, this shows if data is still being loaded.
+    inspectPointLoading: false,
+    // If a point is selected for inspection, the loaded data will be stored here.
+    inspectPointData: null,
+
+    filterValue: null,
+  },
+};
