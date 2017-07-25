7809f3d781bba087bc447600290f17e8e55bef35
Xingchen Hong <hello@xc-h.net>
2017-07-25 14:02:55 -0500

    Tweak workspace layout and refactor style classes.

--- meteor-app/imports/ui/pages/workspace/component.js
+++ meteor-app/imports/ui/pages/workspace/component.js
@@ -237,44 +237,75 @@ export default class WorkspacePage extends React.Component {
           </div>
         ) : null}
 
-        <div className="section_filter">
+        <div className="section-map">
+          <div className="side-panel">
 
-          <div className="filter-row">
-            <label>Year: </label>
-            <input
-              className="layout_fill"
-              type="range"
-              min={rangeMin}
-              max={rangeMax}
-              step="1"
-              value={filterValue}
-              onChange={this._bound_rangeFilterOnChange}
-            />
-            <button onClick={this._bound_yearStepBackButtonOnClick}>&lt;</button>
-            <input type="text" value={filterValue} onChange={this._bound_rangeFilterOnChange} />
-            <button onClick={this._bound_yearStepForwardButtonOnClick}>&gt;</button>
+            <fieldset className="side-panel__section map-animation-controls">
+              <div className="field--year">
+                <label>Year: </label>
+                <input
+                  className="input-slider--year"
+                  type="range"
+                  min={rangeMin}
+                  max={rangeMax}
+                  step="1"
+                  value={filterValue}
+                  onChange={this._bound_rangeFilterOnChange}
+                />
+                <button
+                  className="action--prev-year"
+                  onClick={this._bound_yearStepBackButtonOnClick}
+                >&lt;</button>
+                <input
+                  className="input--year"
+                  type="text"
+                  value={filterValue}
+                  onChange={this._bound_rangeFilterOnChange}
+                />
+                <button
+                  className="action--next-year"
+                  onClick={this._bound_yearStepForwardButtonOnClick}
+                >&gt;</button>
+              </div>
+            </fieldset>
+
+            <fieldset className="side-panel__section layer-list">
+              <legend>Layers</legend>
+              {layers.map((layer, layerIndex) => (
+                <div
+                  key={layerIndex}
+                  className="layer-list__item"
+                >
+                  <div className="layer-title-row">
+                    <input
+                      title="Toggle Visibility"
+                      type="checkbox"
+                      checked={!layer.invisible}
+                      data-layer-index={layerIndex}
+                      onChange={this._bound_layerVisibilityOnChange}
+                    />
+                    <label className="layer-title-label">{layer.name}</label>
+                  </div>
+                  <div className="layer-opacity-row">
+                    <label>Opacity: </label>
+                    <input
+                      className="input-slider--layer-opacity"
+                      type="range"
+                      min="0"
+                      max="255"
+                      step="1"
+                      value={layer.opacity * 255}
+                      data-layer-index={layerIndex}
+                      onChange={this._bound_layerOpacityOnChange}
+                    />
+                    <label>{layer.opacity.toFixed(2)}</label>
+                  </div>
+                </div>
+              ))}
+            </fieldset>
+
           </div>
 
-          <ul className="layer-list">
-            <p>Layer list:</p>
-            {layers.map((layer, layerIndex) => (
-              <li key={layerIndex}>
-                <div>
-                  <input title="Toggle Visibility" type="checkbox" checked={!layer.invisible} data-layer-index={layerIndex} onChange={this._bound_layerVisibilityOnChange} />
-                  <label>{layer.name}</label>
-                </div>
-                <div className="layer-opacity">
-                  <label>Opacity: </label>
-                  <input type="range" min="0" max="255" step="1" value={layer.opacity * 255} data-layer-index={layerIndex} onChange={this._bound_layerOpacityOnChange} />
-                  <label>{layer.opacity.toFixed(2)}</label>
-                </div>
-              </li>
-            ))}
-          </ul>
-
-        </div>
-
-        <div className="section_map">
           <map-view
             class="the-map"
             basemap="osm"
@@ -282,6 +313,7 @@ export default class WorkspacePage extends React.Component {
             zoom="5"
             ref={ref => this._mapview = ref}
           >
+
             {layers.map(o => o.element)}
 
             <map-layer-singlepoint
@@ -293,8 +325,11 @@ export default class WorkspacePage extends React.Component {
             <map-control-defaults />
             <map-interaction-defaults />
             <map-control-simple-layer-list />
+
           </map-view>
+
         </div>
+
       </div>
     );
   }
