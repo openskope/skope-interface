ff6c1f0559ad9a021d767a6e59bd5e501a7e01e9
CiciMa <xxmdaily@gmail.com>
2017-07-27 17:04:27 -0500

    add new data and new filters

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,4 +1,6 @@
 // Client entry point, imports all client code
 
+import '/node_modules/material-components-web/dist/material-components-web.css';
+import '/node_modules/muicss/dist/css/mui.css';
 import '/imports/startup/client';
 import '/imports/startup/both';
--- meteor-app/imports/ui/pages/search/component.js
+++ meteor-app/imports/ui/pages/search/component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Appbar, Divider, Container, Row, Col } from 'muicss/react';
 
 import {
   SearchkitManager,
@@ -22,14 +23,74 @@ import {
 import '/node_modules/searchkit/release/theme.css';
 
 class SearchResultItem extends React.Component {
-  render () {
-    return (
-      <div style={{ overflow: 'auto' }}>
-        <p>Some Result (Implement this)</p>
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
-      </div>
-    );
-  }
+    render () {
+        const {
+            result: {
+                _source: {
+                    Dataset,
+                    Title,
+                    ModelName,
+                    Creator,
+                    Contributors,
+                    CreationDate,
+                    Status,
+                    Rating,
+                    Keywords,
+                    ResultTypes,
+                    StartDate,
+                    EndDate,
+                    Area,
+                    Inputs,
+                    Description,
+                    Info,
+                    Reference,
+                    Workspace,
+                    Download,
+                    Create,
+                },
+            },
+        } = this.props;
+
+        // const _index = this.props.result._index;
+        // const _type = this.props.result._type;
+        // const account_number = this.props.result._source.account_number;
+
+        return (
+            // <div style={{overflow: "auto"}}>
+
+            <div className="container">
+              <div className="result-container">
+                <Appbar className="appbar">
+                  <div className="header">{ModelName}</div>
+                  <div className="date">Run date: {CreationDate}</div>
+                </Appbar>
+                <div className="column">
+                  <div className="column-item">
+                    <img src="http://www.openskope.org/wp-content/uploads/2016/02/ScreenShot001.bmp"></img>
+                  </div>
+                  <div className="column-item">
+                    <p><b>Creator:</b>{Creator}</p>
+                    <p><b>Status:</b>{Status}</p>
+                  </div>
+                  <div className="column-item">
+                    <p><b>Rating:</b>{Rating}</p>
+                  </div>
+                </div>
+                <div className="button">
+                  <Button className="button-item" color="primary">View Data</Button>
+                  <Button className="button-item" color="primary">Download</Button>
+                  <Button className="button-item" color="primary">More Information</Button>
+                </div>
+              </div>
+
+                <div style={{ overflow: 'auto' }}>
+                    <p>Some Result (Implement this)</p>
+                    <pre>{JSON.stringify(this.props, null, 2)}</pre>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default class SearchPage extends React.Component {
@@ -42,89 +103,167 @@ export default class SearchPage extends React.Component {
   render () {
     const {
       searchkit,
+      CreationDate,
     } = this.props;
+      return (
+          <SearchkitProvider searchkit={searchkit}>
+            <div className="page--search">
+              <div className="page--search__sidepanel">
+                <InputFilter
+                    id="category-input"
+                    title="Search by Catgory"
+                    placeholder="PaleoClimate"
+                    searchOnChange
+                    prefixQueryFields={['paleoclimate']}
+                    queryFields={['paleoclimate',]}
+                />
+                <RefinementListFilter
+                    id="modelname-list"
+                    title="Model Name"
+                    field="ModelName"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={4}
+                />
+                <Divider></Divider>
+                <RefinementListFilter
+                    id="creator-list"
+                    title="Creator"
+                    field="Creator"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={4}
+                />
+                  <Divider></Divider>
+                <RefinementListFilter
+                    id="creationdate-list"
+                    title="Creation Date"
+                    field="CreationDate"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={4}
+                />
+                <Divider></Divider>
+                <RefinementListFilter
+                    id="startdate-list"
+                    title="Start Date"
+                    field="StartDate"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={4}
+                />
+                  <RefinementListFilter
+                      id="enddate-list"
+                      title="End Date"
+                      field="EndDate"
+                      operator="OR"
+                      orderKey="_term"
+                      orderDirection="asc"
+                      size={4}
+                  />
+                  <NumericRefinementListFilter
+                  id="creationdate-refine"
+                  title="Creation Date"
+                  field="CreationDate"
+                  options={[
+                  { title: 'All' },
+                  { title: 'up to 1', from: 0, to: 1 },
+                  { title: '1 to 2', from: 1, to: 2 },
+                  { title: '2 to 3', from: 2, to: 3 },
+                  { title: '3 to 4', from: 3, to: 4 },
+                  { title: '4 to 5', from: 4, to: 5 },
+                  ]}
+                  />
+                <RefinementListFilter
+                    id="status-list"
+                    title="Status"
+                    field="Status"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={5}
+                />
+                <RefinementListFilter
+                    id="rating-list"
+                    title="Rating"
+                    field="Rating"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={5}
+                />
+                <RangeFilter
+                    id="rating-range"
+                    field="Rating"
+                    min={0}
+                    max={10}
+                    showHistogram
+                    title=""
+                />
+                <RangeFilter
+                    id="creationdate-range"
+                    field="CreationDate"
+                    min={0}
+                    max={200}
+                    showHistogram
+                    title="Creation Date"
+                />
+                <RefinementListFilter
+                    id="resultTypes-list"
+                    title="Result Types"
+                    field="ResultTypes"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={5}
+                />
+                <RefinementListFilter
+                    id="inputs-list"
+                    title="Input"
+                    field="Inputs"
+                    operator="OR"
+                    orderKey="_term"
+                    orderDirection="asc"
+                    size={5}
+                />
+              </div>
 
-    return (
-      <SearchkitProvider searchkit={searchkit}>
-        <div className="page--search">
-          <div className="page--search__sidepanel">
-            <InputFilter
-              id="lastname-input"
-              title="Search by last name"
-              placeholder="Appleseed"
-              searchOnChange
-              prefixQueryFields={['lastname']}
-              queryFields={['lastname']}
-            />
-            <RefinementListFilter
-              id="state-list"
-              title="State"
-              field="state"
-              operator="OR"
-              size={5}
-            />
-            <NumericRefinementListFilter
-              id="age-refine"
-              title="Age Groups"
-              field="age"
-              options={[
-                { title: 'All' },
-                { title: 'up to 20', from: 0, to: 21 },
-                { title: '21 to 40', from: 21, to: 41 },
-                { title: '41 to 60', from: 41, to: 61 },
-                { title: '61 to 80', from: 61, to: 81 },
-                { title: '81 to 100', from: 81, to: 101 },
-              ]}
-            />
-            <RangeFilter
-              field="age"
-              id="age-range"
-              min={0}
-              max={100}
-              showHistogram
-              title=""
-            />
-            <RefinementListFilter
-              id="employer-list"
-              title="Employer"
-              field="employer"
-              operator="OR"
-              size={5}
-            />
-          </div>
+              <div className="page--search__searchpanel">
 
-          <div className="page--search__searchpanel">
+                <SearchBox
+                    autofocus
+                    searchOnChange
+                    prefixQueryFields={['actors^1', 'type^2', 'languages', 'title^10']}
+                />
 
-            <SearchBox
-              autofocus
-              searchOnChange
-              prefixQueryFields={['actors^1', 'type^2', 'languages', 'title^10']}
-            />
+                <LayoutResults>
+                  <ActionBar>
 
-            <LayoutResults>
-              <ActionBar>
+                    <ActionBarRow>
+                      <HitsStats />
+                    </ActionBarRow>
 
-                <ActionBarRow>
-                  <HitsStats />
-                </ActionBarRow>
+                    <ActionBarRow>
+                      <SelectedFilters />
+                      <ResetFilters />
+                    </ActionBarRow>
 
-                <ActionBarRow>
-                  <SelectedFilters />
-                  <ResetFilters />
-                </ActionBarRow>
+                  </ActionBar>
+                  <Hits mod="sk-hits-grid" hitsPerPage={3} itemComponent={SearchResultItem} />
+                  <NoHits />
 
-              </ActionBar>
-              <Hits mod="sk-hits-grid" hitsPerPage={10} itemComponent={SearchResultItem} />
-              <NoHits />
+                  <Pagination
+                      showNumbers
+                  />
+                </LayoutResults>
 
-              <Pagination
-                showNumbers
-              />
-            </LayoutResults>
-
-          </div>
-        </div>
-      </SearchkitProvider>
-    );
+              </div>
+            </div>
+          </SearchkitProvider>
+      );
   }
 }
