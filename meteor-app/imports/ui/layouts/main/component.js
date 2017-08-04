6e9af040cc012b1ecedb8c104b19c47cdefbdddc
Piotrek605 <piotrw605@gmail.com>
2017-08-04 12:23:08 -0500

    add help button

--- meteor-app/imports/ui/layouts/main/component.js
+++ meteor-app/imports/ui/layouts/main/component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Button } from 'muicss/react';
 import Navbar from '/imports/ui/components/navbar/container';
 
 import {
@@ -11,19 +11,22 @@ export default class MainLayout extends React.Component {
     const {
       store,
       body,
+      helpUrl,
     } = this.props;
+
     return (
       <div className="page layout-main">
         <div className="page__header">
-          <p>Some header</p>
           <Navbar store={store} />
         </div>
         <div className="page__body">
           {body}
         </div>
         <div className="page__footer">
-          <p>Some footer</p>
-          <p><a href={demoRepository} target="_blank">Source code on Github</a></p>
+          <a href={helpUrl}>
+            <Button color="primary" size="small">Help</Button>
+          </a>
+          <a href={demoRepository} target="_blank">Source code on Github</a>
         </div>
       </div>
     );
