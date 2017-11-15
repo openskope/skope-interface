e454c3d855beea5ac7955badc7e9ab77e51ffa53
Xingchen Hong <hello@xc-h.net>
2017-11-15 16:56:48 -0600

    Override default filter items with Material Design Chips.

--- meteor-app/imports/ui/pages/search-by-map/component.js
+++ meteor-app/imports/ui/pages/search-by-map/component.js
@@ -12,6 +12,7 @@ import Paper from 'material-ui/Paper';
 import SpatialFilter from '/imports/ui/components/searchkit-spatial-filter';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
+import Chip from 'material-ui/Chip';
 import moment from 'moment';
 
 import {
@@ -30,7 +31,7 @@ import {
   NoHits,
 } from 'searchkit';
 
-class SearchResultItem extends React.Component {
+class SearchResultItem extends React.PureComponent {
 
   static getDateRange (start, end) {
     if (!start && !end) {
@@ -113,6 +114,73 @@ class SearchResultItem extends React.Component {
   }
 }
 
+const DynamicDateRangeFormatter =
+(labelValue) => labelValue
+.split(' - ')
+.map((timeString) => parseInt(timeString, 10))
+.map((timestamp) => moment(timestamp).format('YYYY-MM-DD'))
+.join(' - ');
+
+const LabelValueFormatters = {
+  'Start Date': DynamicDateRangeFormatter,
+  'End Date': DynamicDateRangeFormatter,
+};
+
+class FilterItem extends React.PureComponent {
+  static propTypes = {
+    bemBlocks: PropTypes.object.isRequired,
+    filterId: PropTypes.string.isRequired,
+    labelKey: PropTypes.string.isRequired,
+    labelValue: PropTypes.string.isRequired,
+    removeFilter: PropTypes.func.isRequired,
+  };
+
+  static formatLabel = (labelKey, labelValue) => {
+    if (!(labelKey in LabelValueFormatters)) {
+      return labelValue;
+    }
+
+    const formattedLabelValue = LabelValueFormatters[labelKey](labelValue);
+
+    return formattedLabelValue
+    ? `${labelKey}: ${formattedLabelValue}`
+    : labelKey;
+  };
+
+  render () {
+    const {
+      bemBlocks,
+      filterId,
+      labelKey,
+      labelValue,
+      removeFilter,
+    } = this.props;
+
+    const containerClassName =
+    bemBlocks.option()
+    .mix(bemBlocks.container('item'))
+    .mix(`selected-filter--${filterId}`)
+    .toString();
+    const formattedLabel = this.constructor.formatLabel(labelKey, labelValue);
+
+    return (
+      <div
+        className={containerClassName}
+        style={{
+          background: 'transparent',
+          padding: 0,
+        }}
+      >
+        <Chip
+          onRequestDelete={removeFilter}
+        >
+          <div className={bemBlocks.option('name')}>{formattedLabel}</div>
+        </Chip>
+      </div>
+    );
+  }
+}
+
 export default class SearchPage extends React.Component {
 
   static propTypes = {
@@ -165,17 +233,16 @@ export default class SearchPage extends React.Component {
             <div className="page-search__result">
               <LayoutResults>
                 <ActionBar>
-
                   <ActionBarRow>
                     <HitsStats />
                   </ActionBarRow>
 
                   <ActionBarRow>
-                    <SelectedFilters />
+                    <SelectedFilters itemComponent={FilterItem} />
                     <ResetFilters />
                   </ActionBarRow>
-
                 </ActionBar>
+
                 <Hits mod="sk-hits-grid" hitsPerPage={3} itemComponent={SearchResultItem} />
                 <NoHits />
 
