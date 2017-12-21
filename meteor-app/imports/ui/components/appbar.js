7364bd2947f424a9dc6f05b53dd88039ef85fde2
Xingchen Hong <zodiase@gmail.com>
2017-12-21 12:06:15 -0600

    Add login controls (#87)
    - Turn account button into a menu with disabled "log in" and "sign up" options.
    - Move appbar constants to app settings.
    - Turn the app bar component into a component class.
    
    This resolves #44.

--- meteor-app/imports/ui/components/appbar.js
+++ meteor-app/imports/ui/components/appbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   connect,
 } from 'react-redux';
@@ -7,6 +8,7 @@ import IconButton from 'material-ui/IconButton';
 import Drawer from 'material-ui/Drawer';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import ArrowRightIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 import HelpIcon from 'material-ui/svg-icons/action/help';
@@ -22,119 +24,129 @@ import {
   appSettings,
 } from '/package.json';
 
-const APPBAR_BACKGROUNDCOLOR = '#f7f5e7';
-const APPBAR_COLOR = 'black';
+class Component extends React.Component {
+  static propTypes = {
+    title: PropTypes.any,
+    children: PropTypes.any,
+    currentRouterPath: PropTypes.string,
+    drawerIsOpen: PropTypes.bool,
+    onClickHamburgerButton: PropTypes.func,
+    setDrawerOpenState: PropTypes.func,
+    navigateTo: PropTypes.func,
+    onClickHelpButton: PropTypes.func,
+  };
 
-const Component = ({
-  /**
-   * @type {*}
-   */
-  title = null,
-  /**
-   * @type {*}
-   */
-  children = null,
-  /**
-   * @type {String}
-   */
-  currentRouterPath = '',
-  /**
-   * @type {Boolean}
-   */
-  drawerIsOpen = false,
-  /**
-   * @type {Function}
-   */
-  onClickHamburgerButton,
-  /**
-   * @type {Function}
-   */
-  setDrawerOpenState,
-  /**
-   * @type {Function}
-   */
-  navigateTo,
-  /**
-   * Do whatever you want when the help button is clicked.
-   * @type {Function}
-   */
-  onClickHelpButton,
-}) => (
-  <div className="appbar">
-    <AppBar
+  static defaultProps = {
+    title: null,
+    children: null,
+    currentRouterPath: '',
+    drawerIsOpen: false,
+    onClickHamburgerButton: () => {},
+    setDrawerOpenState: () => {},
+    navigateTo: () => {},
+    onClickHelpButton: () => {},
+  };
+
+  renderTitle = () => (
+    <div
       style={{
-        backgroundColor: APPBAR_BACKGROUNDCOLOR,
+        color: appSettings.appBarTextColor,
       }}
-      title={
-        <div
-          style={{
-            color: APPBAR_COLOR,
-          }}
-        >
-          <span style={{ marginRight: 30 }}>SKOPE</span>
-          {title}
-        </div>
-      }
-      iconElementLeft={
-        <IconButton
-          tooltip="Menu"
-          onClick={onClickHamburgerButton}
-        ><MenuIcon color={APPBAR_COLOR} /></IconButton>
-      }
-      iconElementRight={
-        <span>
-          <IconButton
-            tooltip="Notifications"
-          ><NoNotificationIcon color={APPBAR_COLOR} /></IconButton>
+    >
+      <span style={{ marginRight: 30 }}>SKOPE</span>
+      {this.props.title}
+    </div>
+  );
 
+  renderLeftIcon = () => (
+    <IconButton
+      tooltip="Menu"
+      onClick={this.props.onClickHamburgerButton}
+    ><MenuIcon color={appSettings.appBarTextColor} /></IconButton>
+  );
+
+  renderToolbar = () => (
+    <span>
+      <IconButton
+        tooltip="Notifications"
+      ><NoNotificationIcon color={appSettings.appBarTextColor} /></IconButton>
+
+      <IconMenu
+        iconButtonElement={
           <IconButton
             tooltip="Account"
-          ><AccountIcon color={APPBAR_COLOR} /></IconButton>
+          ><AccountIcon color={appSettings.appBarTextColor} /></IconButton>
+        }
+        targetOrigin={{
+          horizontal: 'middle',
+          vertical: 'top',
+        }}
+        anchorOrigin={{
+          horizontal: 'middle',
+          vertical: 'bottom',
+        }}
+      >
+        <MenuItem disabled>Log in</MenuItem>
+        <Divider />
+        <MenuItem disabled>Sign up</MenuItem>
+      </IconMenu>
+
+      <IconButton
+        tooltip="Help"
+        onClick={this.props.onClickHelpButton}
+      ><HelpIcon color={appSettings.appBarTextColor} /></IconButton>
+
+      <IconMenu
+        iconButtonElement={
           <IconButton
-            tooltip="Help"
-            onClick={onClickHelpButton}
-          ><HelpIcon color={APPBAR_COLOR} /></IconButton>
+            tooltip="Boring Stuff"
+            tooltipPosition="bottom-left"
+          ><CodeIcon color={appSettings.appBarTextColor} /></IconButton>
+        }
+        targetOrigin={{
+          horizontal: 'right',
+          vertical: 'top',
+        }}
+        anchorOrigin={{
+          horizontal: 'right',
+          vertical: 'bottom',
+        }}
+      >
+        <MenuItem>
+          <a href={demoRepository} target="_blank" rel="noopener noreferrer">{`ver ${version}`}</a>
+        </MenuItem>
+      </IconMenu>
+    </span>
+  );
 
-          <IconMenu
-            iconButtonElement={
-              <IconButton
-                tooltip="Boring Stuff"
-                tooltipPosition="bottom-left"
-              ><CodeIcon color={APPBAR_COLOR} /></IconButton>
-            }
-            targetOrigin={{
-              horizontal: 'right',
-              vertical: 'top',
-            }}
-            anchorOrigin={{
-              horizontal: 'right',
-              vertical: 'bottom',
-            }}
-          >
-            <MenuItem>
-              <a href={demoRepository} target="_blank" rel="noopener noreferrer">{`ver ${version}`}</a>
-            </MenuItem>
-          </IconMenu>
-        </span>
-      }
-    >{children}</AppBar>
+  render = () => (
+    <div className="appbar">
+      <AppBar
+        style={{
+          backgroundColor: appSettings.appBarBackgroundColor,
+        }}
+        title={this.renderTitle()}
+        iconElementLeft={this.renderLeftIcon()}
+        iconElementRight={this.renderToolbar()}
+      >{this.props.children}</AppBar>
 
-    <Drawer
-      docked={false}
-      width={appSettings.drawerWidth}
-      open={drawerIsOpen}
-      onRequestChange={setDrawerOpenState}
-    >
-      {appSettings.drawerItems.map((item) => (
-        <MenuItem
-          key={item.title}
-          onClick={() => navigateTo(item.url)}
-          leftIcon={currentRouterPath === item.url ? <ArrowRightIcon /> : null}
-        >{item.title}</MenuItem>
-      ))}
-    </Drawer>
-  </div>
-);
+      <Drawer
+        docked={false}
+        width={appSettings.drawerWidth}
+        open={this.props.drawerIsOpen}
+        onRequestChange={this.props.setDrawerOpenState}
+      >
+        {appSettings.drawerItems.map((item) => (
+          <MenuItem
+            key={item.title}
+            onClick={() => this.props.navigateTo(item.url)}
+            leftIcon={this.props.currentRouterPath === item.url ? <ArrowRightIcon /> : null}
+          >{item.title}</MenuItem>
+        ))}
+      </Drawer>
+    </div>
+  );
+}
 
 export default connect(
   // mapStateToProps
