de66baeaa25e075d79594b606b8668f10051441f
Xingchen Hong <hello@xc-h.net>
2018-01-29 16:53:35 -0600

    Only use the `UnknownTypeRenderer` when configured in the app settings.

--- meteor-app/imports/ui/pages/search-by-map/RenderSearchResultItemByType.js
+++ meteor-app/imports/ui/pages/search-by-map/RenderSearchResultItemByType.js
@@ -2,23 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import objectPath from 'object-path';
 
-import {
-  PropPrinter,
-} from '/imports/ui/helpers';
-
 import * as searchResultItemRenderers from './searchResultItemRenderers';
+
+import UnknownTypeRenderer from './searchResultItemRenderers/unknown';
 
 const resultTypeFieldPath = 'result._source.type';
 const renderSearchResultItemsWithUnknownType = objectPath.get(Meteor.settings, 'public.renderSearchResultItemsWithUnknownType', false);
 const renderInvalidSearchResultItems = objectPath.get(Meteor.settings, 'public.renderInvalidSearchResultItems', false);
 
 export default (props) => {
-  const resultItemType = objectPath.get(props, resultTypeFieldPath, 'unknown');
+  const resultItemType = objectPath.get(props, resultTypeFieldPath);
 
   if (!resultItemType) {
     if (renderInvalidSearchResultItems) {
       //! Use a dedicated component to show an invalid data error.
-      return React.createElement(PropPrinter, props);
+      return React.createElement(UnknownTypeRenderer, props);
     }
 
     return null;
@@ -28,8 +26,7 @@ export default (props) => {
 
   if (!(rendererName in searchResultItemRenderers)) {
     if (renderSearchResultItemsWithUnknownType) {
-      //! Use a dedicated component to show an unknown type error.
-      return React.createElement(PropPrinter, props);
+      return React.createElement(UnknownTypeRenderer, props);
     }
 
     return null;
--- meteor-app/imports/ui/pages/search-by-map/searchResultItemRenderers/index.js
+++ meteor-app/imports/ui/pages/search-by-map/searchResultItemRenderers/index.js
@@ -3,6 +3,5 @@
  * so the component files don't need to worry about name conflicts.
  */
 
-export SEARCH_RESULT_ITEM__UNKNOWN from './unknown';
 export SEARCH_RESULT_ITEM__DEFAULT from './default';
 export SEARCH_RESULT_ITEM__PALEOCARRUN from './paleocar-run';
