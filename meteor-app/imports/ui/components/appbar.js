7b1d6560d6cca9cc10aabb5fc10d7e875f00b428
Xingchen Hong <hello@xc-h.net>
2018-03-29 15:07:30 -0500

    Remove the drawer (the hamburger menu).

--- meteor-app/imports/ui/components/appbar.js
+++ meteor-app/imports/ui/components/appbar.js
@@ -5,12 +5,9 @@ import {
 } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import Drawer from 'material-ui/Drawer';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
-import ArrowRightIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
-import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 import HelpIcon from 'material-ui/svg-icons/action/help';
 import AccountIcon from 'material-ui/svg-icons/action/account-circle';
 import NoNotificationIcon from 'material-ui/svg-icons/social/notifications-none';
@@ -31,9 +28,6 @@ class Component extends React.Component {
     title: PropTypes.any,
     children: PropTypes.any,
     currentRouterPath: PropTypes.string,
-    drawerIsOpen: PropTypes.bool,
-    onClickHamburgerButton: PropTypes.func,
-    setDrawerOpenState: PropTypes.func,
     navigateTo: PropTypes.func,
     onClickHelpButton: PropTypes.func,
   };
@@ -42,9 +36,6 @@ class Component extends React.Component {
     title: null,
     children: null,
     currentRouterPath: '',
-    drawerIsOpen: false,
-    onClickHamburgerButton: () => {},
-    setDrawerOpenState: () => {},
     navigateTo: () => {},
     onClickHelpButton: () => {},
   };
@@ -66,13 +57,6 @@ class Component extends React.Component {
       />
       {this.props.title}
     </div>
-  );
-
-  renderLeftIcon = () => (
-    <IconButton
-      tooltip="Menu"
-      onClick={this.props.onClickHamburgerButton}
-    ><MenuIcon color={appSettings.appBarTextColor} /></IconButton>
   );
 
   renderToolbar = () => (
@@ -150,24 +134,9 @@ class Component extends React.Component {
           backgroundColor: appSettings.appBarBackgroundColor,
         }}
         title={this.renderTitle()}
-        iconElementLeft={this.renderLeftIcon()}
+        showMenuIconButton={false}
         iconElementRight={this.renderToolbar()}
       >{this.props.children}</AppBar>
-
-      <Drawer
-        docked={false}
-        width={appSettings.drawerWidth}
-        open={this.props.drawerIsOpen}
-        onRequestChange={this.props.setDrawerOpenState}
-      >
-        {appSettings.drawerItems.map((item) => (
-          <MenuItem
-            key={item.title}
-            onClick={() => this.props.navigateTo(item.url)}
-            leftIcon={this.props.currentRouterPath === item.url ? <ArrowRightIcon /> : null}
-          >{item.title}</MenuItem>
-        ))}
-      </Drawer>
     </div>
   );
 }
@@ -176,17 +145,9 @@ export default connect(
   // mapStateToProps
   (state) => ({
     currentRouterPath: state.path,
-    drawerIsOpen: state.drawer.isOpen,
   }),
   // mapDispatchToProps
   (dispatch) => ({
-    onClickHamburgerButton: () => dispatch({
-      type: actions.OPEN_DRAWER.type,
-    }),
-    setDrawerOpenState: (isOpen) => dispatch({
-      type: actions.OPEN_DRAWER.type,
-      overrideWith: isOpen,
-    }),
     navigateTo: (url) => dispatch({
       type: actions.CHANGE_ROUTE.type,
       path: url,
