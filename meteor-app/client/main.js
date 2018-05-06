65343bfda455c4ca004386800b96a43c7997324b
Xingchen Hong <hello@xc-h.com>
2018-05-06 04:36:17 -0500

    Load the latest `web-gis-elements` from npm instead of submodule.

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,7 +1,8 @@
 // Client entry point, imports all client code
 
 import 'meteor/zodiase:mdc/stylesheet';
-import '/public/web-gis-components/web-gis-components.js';
+
+import '@xch/web-gis-elements/dist/web-gis-elements.js';
 
 import '/imports/startup/client';
 import '/imports/startup/both';
