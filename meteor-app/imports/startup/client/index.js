f1df5942c886413f8950fbeaf2d65e754d6c268d
Xingchen Hong <hello@xc-h.com>
2018-05-16 14:38:15 -0500

    Setup error tracking with Sentry.
    Need Sentry DSN to be stored at `sentry.dsn` and `public.sentry.dsn` for server side and client side, respectively.

--- meteor-app/imports/startup/client/index.js
+++ meteor-app/imports/startup/client/index.js
@@ -1,4 +1,20 @@
 // Import client startup through a single index entry point
 
+import { Meteor } from 'meteor/meteor';
+import objectPath from 'object-path';
+import Raven from 'raven-js';
 import 'typeface-roboto';
+
 import './routes.js';
+
+((sentryDsn) => {
+  if (!sentryDsn) {
+    return;
+  }
+
+  Raven.config(sentryDsn, {
+    logger: 'client',
+  }).install();
+
+  console.log('Sentry is up');
+})(objectPath.get(Meteor.settings, 'public.sentry.dsn'));
--- meteor-app/imports/startup/server/index.js
+++ meteor-app/imports/startup/server/index.js
@@ -1,3 +1,19 @@
 // Import server startup through a single index entry point
 
+import { Meteor } from 'meteor/meteor';
+import objectPath from 'object-path';
+import Raven from 'raven';
+
 import './register-api.js';
+
+((sentryDsn) => {
+  if (!sentryDsn) {
+    return;
+  }
+
+  Raven.config(sentryDsn, {
+    logger: 'server',
+  }).install();
+
+  console.log('Sentry is up');
+})(objectPath.get(Meteor.settings, 'sentry.dsn'));
